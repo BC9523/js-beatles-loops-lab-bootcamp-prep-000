4eda89f94575eb66f5f7428a1c2b9b77d4c5d777
Calypso Day <calypso.day@yahoo.com>
2019-04-09 15:02:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function theBeatlesPlay(arrayMusicians, arrayInstruments){
   var whoPlays = [];
   var string;
   for(i = 0; i < 4; i++) {
-    
+    string = `${arrayMusicians[i]} plays ${arrayInstruments[i]}`;
   }  
 }
